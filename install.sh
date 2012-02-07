08c8e2abee0edbfa2a9d9472a63e04cc76ace612
Willian Fernandes <willian@willianfernandes.com.br>
2012-02-07 14:30:51 -0200

    added taskpaper bundle

--- install.sh
+++ install.sh
@@ -50,6 +50,7 @@ get_bundle charlesr ruby-sass-tmbundle "Bundles/Ruby Sass.tmbundle"
 get_bundle handcrafted handcrafted-haml-textmate-bundle "Bundles/HAML-Handcrafted.tmbundle"
 get_bundle willian wfernandes-tmbundle "Bundles/wfernandes.tmbundle"
 get_bundle johnmuhl javascript-tools-tmbundle "Bundles/JavaScriptTools.tmbundle"
+get_bundle jessmartin taskpaper-tmbundle "Bundles/taskpaper.tmbundle"
 
 sudo ln -s /System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc /usr/local/bin
 
